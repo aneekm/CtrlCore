b00477bcc5feea86591542501db64d972e695451
aneekm <aneek.914@gmail.com>
2017-02-11 06:33:29 -0500

    Updated Arduino Code
    Simplified the Serial input (for now) - accepts 9 digits, 3 each for R,
    G, B 000-255.

--- TartanHacks/TartanHacks.ino
+++ TartanHacks/TartanHacks.ino
@@ -6,18 +6,17 @@
 
 //constants for the serial input processing and LED strip
 String input;
-int brightness = 255;
 int red = 0;
 int green = 0;
 int blue = 0;
 uint32_t color = 0;
-int arr[LED] = { 0 };
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(100, PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(57600);
   strip.begin();
+  strip.setBrightness(200);
   strip.show();
   input.reserve(1000);
 }
@@ -30,26 +29,17 @@ void loop() {
   while (Serial.available() > 0) {
     input = Serial.readStringUntil('\n');
     Serial.println(input);
-    if (input.startsWith("B")) {
-      brightness = input.substring(1).toInt();
-    } else if (input.startsWith("C")) {
-      red = input.substring(1, 4).toInt();
-      green = input.substring(4, 7).toInt();
-      blue = input.substring(7).toInt();
-    }
+    red = input.substring(0, 3).toInt();
+    green = input.substring(3, 6).toInt();
+    blue = input.substring(6).toInt();
   }   
-
-  //Code to adjust the rgb values by brightness
-  red = brightness*red/100;
-  green = brightness*green/100;
-  blue - brightness*blue/100;  
-
-  //Code that pushes the color to the led strip
+  
+  //Code that sets the color for each pixel in the strip
   color = strip.Color(red, green, blue);
   for(int i=0;i<LED;i++) {
     strip.setPixelColor(i, color);
   }
 
+  //pushes the colors to the strip
   strip.show();
-  
 }
